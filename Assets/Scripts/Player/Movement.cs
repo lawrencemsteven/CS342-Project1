7777f409df4943086dd70b246733de12e6a83089
chrisflune <94500466+chrisflune@users.noreply.github.com>
2023-01-29 02:14:16 -0500

    player animation

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -13,11 +13,27 @@ public class Movement : MonoBehaviour
 
     private Office office;
     private Supply supply;
+<<<<<<< Updated upstream
 
     void Start()
     {
         lights = maxLights;
 
+=======
+    public LightBulb lights;
+    public StarRating stars;
+    public Money money;
+    public Animator animator;
+
+    private SpriteRenderer sprite;
+
+    public OfficeManager officeManager;
+
+    private int fixCount = 0;
+
+    void Start()
+    {
+>>>>>>> Stashed changes
         rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
     }
@@ -30,9 +46,44 @@ public class Movement : MonoBehaviour
 
         int left = Input.GetKey(KeyCode.A) ? 1 : 0;
         int right = Input.GetKey(KeyCode.D) ? 1 : 0;
+<<<<<<< Updated upstream
         
         rb.velocity = ((up - down) * transform.up) + ((right - left) * transform.right) * speed;
 
+=======
+
+        rb.velocity = (((up - down) * transform.up) + ((right - left) * transform.right)) * speed;
+
+
+        //player animation change direction
+        if (Input.GetKey(KeyCode.W)) {
+          animator.SetBool("up", true);
+        }
+        if (!Input.GetKey(KeyCode.W)) {
+          animator.SetBool("up", false);
+        }
+        if (Input.GetKey(KeyCode.S)) {
+          animator.SetBool("down", true);
+        }
+        if (!Input.GetKey(KeyCode.S)) {
+          animator.SetBool("down", false);
+        }
+        if (Input.GetKey(KeyCode.A)) {
+          animator.SetBool("left", true);
+        }
+        if (!Input.GetKey(KeyCode.A)) {
+          animator.SetBool("left", false);
+        }
+        if (Input.GetKey(KeyCode.D)) {
+          animator.SetBool("right", true);
+        }
+        if (!Input.GetKey(KeyCode.D)) {
+          animator.SetBool("right", false);
+        }
+
+
+        //player install and get lightbulbs
+>>>>>>> Stashed changes
         if (Input.GetKey(KeyCode.E)) {
             if (office != null) {
                 office.installLight();
